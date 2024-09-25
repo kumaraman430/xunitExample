3c76885a636ba8b046f96f7376bfb76bd83c3144
kumaraman430 <ucckumaraman430@gmail.com>
2024-09-25 17:11:38 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace xunitExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet("index/{guessedNumber}")]
+        public string Index(int guessedNumber)
+        {
+            if (guessedNumber < 100)
+            {
+                return "Try a bigger number";
+            }
+            else if (guessedNumber > 100) 
+            {
+                return "Try a smaller number";
+            }
+            else
+            {
+                return "Guessed number is correct";
+            }
+        }
+    }
+}
